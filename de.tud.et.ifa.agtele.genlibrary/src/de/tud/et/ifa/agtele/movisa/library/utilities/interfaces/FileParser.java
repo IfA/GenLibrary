e0afe64f9a075185ab6a3d3402949e3ccbee19dc
mfreund <matth.freund@gmail.com>
2014-12-02 15:20:33 +0100

    Add a comment

--- de.tud.et.ifa.agtele.genlibrary/src/de/tud/et/ifa/agtele/movisa/library/utilities/interfaces/FileParser.java
+++ de.tud.et.ifa.agtele.genlibrary/src/de/tud/et/ifa/agtele/movisa/library/utilities/interfaces/FileParser.java
@@ -8,6 +8,15 @@ import de.tud.et.ifa.agtele.movisa.library.model.Library.Item;
 
 public interface FileParser {
 
+	/**
+	 * This returns the library entries inside a library (represented by a
+	 * Zip/Jar file).
+	 * 
+	 * @param file
+	 *            A path pointing to a library file (Zip/Jar).
+	 * @return A list of entries inside the library.
+	 * @throws IOException
+	 */
 	public List<LibraryFileEntry> parseFile(Path file) throws IOException;
 
 	public LibraryFileEntry parseFile(Item item) throws IOException;
